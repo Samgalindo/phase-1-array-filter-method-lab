b9ae981978bb3824af82faefa557d9bf13517f71
Sam Galindo <ovosamgalindo@gmail.com>
2023-04-01 20:20:35 -0700

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    return drivers.filter(drivers => drivers.toLowerCase()=== name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver => driver.slice(0, letters.length).toLowerCase()=== letters.toLowerCase())
+}
+
+function matchName(drivers, targetName){
+    return drivers.filter(driver => driver.name === targetName)
+}
